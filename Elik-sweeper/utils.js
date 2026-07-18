6850f5897b9f577945cb2edcb749ff872e510e8d
Elikonchik <elikbelikshluk@gmail.com>
2026-07-19 00:18:42 +0300

    Add files via upload

---
+++ Elik-sweeper/utils.js
@@ -0,0 +1,24 @@
+'use strict'
+
+function forEachNeighbor(board, rowIdx, colIdx, cb) {
+
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+
+        if (i < 0 || i >= board.length) continue
+
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+
+            if (j < 0 || j >= board[0].length) continue
+
+            if (i === rowIdx && j === colIdx) continue
+
+            cb(board[i][j], i, j)
+        }
+    }
+}
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
